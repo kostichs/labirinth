bb311c6da00285ac5ed4e2d0124f271f352b141d
sergey <sergey.kostichev@gmail.com>
2023-11-19 22:44:17 +0500

    Labyrinth displaying 2

--- labirinth.py
+++ labirinth.py
@@ -1,7 +1,20 @@
 import csv
 
-with open("maze.csv", 'r') as file:
-    csvreader = csv.reader(file)
-    for row in csvreader:
-        for col in row:
-            print(col.replace('0;', '  '))
+
+def walk():
+    start = (1, 1)
+    end = (len(matrix) - 2, len(matrix) - 2)
+    matrix[start[0]][start[1]] = 'V'
+    matrix[end[0]][end[1]] = 'X'
+
+with open('maze.csv', 'r') as file:
+    reader = csv.reader(file, delimiter=';')
+    matrix = []
+    for row in reader:
+        matrix.append(row)
+
+matrix = [[' ' if element == '0' else element for element in row] for row in matrix]
+
+walk()
+for row in matrix:
+    print(' '.join(row))
